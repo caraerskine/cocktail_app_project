65c3a64c458495082f7d3379f41076c4c2cb5862
CaraErskine <caraerskine@gmail.com>
2022-07-26 19:19:13 -0400

    tried to fetch again

--- src/index.js
+++ src/index.js
@@ -3,48 +3,43 @@ console.info('src/index.js loaded');
 document.addEventListener('DOMContentLoaded', () => { 
 
 
-//   console.log("DOMContentLoaded")
-// function getNewDrinks(){
-//   fetch('www.thecocktaildb.com/api/json/v1/1/random.php')
-//   .then(res => res.json())
-//   .then(data => console.log (data))
-// }
-
+  console.log("DOMContentLoaded")
 function getNewDrinks(){
-fetch('www.thecocktaildb.com/api/json/v1/1/search.php?i=vodka')
-    .then(
-    function(response) {
-        if (response.status !== 200) {
-        console.log('Looks like there was a problem. Status Code: ' +
-            response.status);
-        return;
-        }
+  fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php:1')
+  .then(res => res.json())
+  .then(data => console.log (data))
+}
 
-        // Examine the text in the response
-        response.json().then(function(data) {
-        console.log(data);
-        });
-    }
-    )
-    .catch(function(err) {
-    console.log('Fetch Error :-S', err);
-    });
+// function getNewDrinks(){
+// fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php:1')
+//     .then(
+//     function(response) {
+//         if (response.status !== 200) {
+//         console.log('Looks like there was a problem. Status Code: ' +
+//             response.status);
+//         return;
+//         }
 
+//         // Examine the text in the response
+//         response.json().then(function(data) {
+//         console.log(data);
+//         });
+//     }
+//     )
+    // .catch(function(err) {
+    // console.log('Fetch Error :-S', err);
+    // });
 
-
-
-
-
-
-
-
-
+  getNewDrinks();
 
 
 
 
+  })
 
-getNewDrinks();
+  
+  //getNewDrinks();
 
 
-  }})
+
+
