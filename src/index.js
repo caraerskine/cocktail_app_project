12c589a2ae82c0f1e9586a6759944278aa5527c8
CaraErskine <caraerskine@gmail.com>
2022-07-29 18:44:12 -0400

    adding data to db.json

--- src/index.js
+++ src/index.js
@@ -20,7 +20,7 @@ console.log("DOMContentLoaded")
  //Alternate fetch I found 
 
 function getNewDrinks(){
-fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php:1')
+fetch('http://www.localhost:3000/cocktails')
     .then(
     function(response) {
         if (response.status !== 200) {
@@ -53,14 +53,14 @@ fetch('http://www.thecocktaildb.com/api/json/v1/1/random.php:1')
   //see a random cocktail
   //clearing the field, is not redirect 
   //assesment by the 12th
-  //1-get all cocntails on one page (60-70% project requirements)
+  //1-get all coktails on one page (60-70% project requirement)
   //2-can the random cocktail button work?
   //could people add their own cocktails?
   //localhost300/cocktails
   //return would be an array of datda
   //for each or a map to craete li's or cards
   //objects should have an id or else back end will have problems
-  // onece you return data you can play wiuth it
+  // once you return data you can play with it
 
   //book a meeting before the assesment 1:1 for a code challenge
 
