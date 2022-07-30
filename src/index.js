bb6162c784539eda499ae7efb73f03575d33b4c4
CaraErskine <caraerskine@gmail.com>
2022-07-30 18:52:39 -0400

    updated db and got data

--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ document.addEventListener('DOMContentLoaded', () => {
 
 fetch('http://www.localhost:3000/cocktails')
 .then(res => res.json())
-.then(getCocktails(data))
+.then(cocktails => cocktails.forEach(cocktail => renderCocktail(cocktail)))
 
 })
-
-function getCocktails(cocktails){
-const {name} = cocktails  
-    console.log(name)
-}
   
-getCocktails(data);
+function renderCocktail(cocktail){
+
+    const {name, desc} = cocktail
+    console.log(name, desc)
+}
 
  
 
