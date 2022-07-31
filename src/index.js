853f345997944c4608b83a2e527069dc20933a39
CaraErskine <caraerskine@gmail.com>
2022-07-31 14:57:49 -0400

    added random drink button, not working, but light/dark toggle is

--- src/index.js
+++ src/index.js
@@ -7,14 +7,37 @@ fetch('http://www.localhost:3000/cocktails')
 .then(cocktails => cocktails.forEach(cocktail => renderCocktail(cocktail)))
 
 })
-  
-function renderCocktail(cocktail){
 
-    const {name, desc} = cocktail
-    console.log(name, desc)
+
+//idk if this is the function
+function randomDrink(cocktail) {
+const randomDrink = cocktail.forEach((item) => {console.log(item)});
 }
 
- 
+function renderCocktail(cocktail){
+
+    const {name, desc, id} = cocktail
+    console.log(name, desc, id)
+}
+
+
+//toggle dark-light mode 
+
+const element = document.getElementById("darkButton");
+
+element.addEventListener("click", () => {
+   
+    document.body.classList.toggle("dark-mode")
+});
+
+//random drink button 
+
+const drinks = document.getElementById("clickme");
+
+drinks.addEventListener("click", () => {
+    document.getElementById("displayme").innerHTML = console.log(Math.floor(Math.random() * renderCocktail));
+}
+)
 
 
 
